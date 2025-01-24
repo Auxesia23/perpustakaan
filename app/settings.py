bd545554fd633ab7899cd37466e3ebec23d8046c
Auxesia23 <fajargustiana2@gmail.com>
2025-01-24 09:23:01 +0700

    add requirements.txt

--- app/settings.py
+++ app/settings.py
@@ -77,7 +77,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  
+#         'NAME': 'tugbes_pbd',              
+#         'USER': 'fajar',          
+#         'PASSWORD': 'fajar123',         
+#         'HOST': '127.0.0.1',                  
+#         'PORT': '3306',                       
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
